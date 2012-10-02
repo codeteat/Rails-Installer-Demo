e92b70a305c064386c7e5120cf5809a8ae1e75b5
Roy Mereness <master@codeteat.org>
2012-10-02 12:05:55 -0500

    Codeteat first ever commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '897ada41f81ca248f8319d2d64c5dc546010a6142d2faf951ed7b13a49d624b80c102df27f51d49d365159cf8876aeff53402b2edbbeccde5d5bd90a866c5f98'
